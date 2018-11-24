e00cba86e46aa19eda705ed694328ee41d938b35
Ron Pinz <ron.pinz@gmail.com>
2018-11-23 19:40:58 -0500

    add manifest for swift 5

---
+++ Package@swift-5.swift
@@ -0,0 +1,35 @@
+// swift-tools-version:5.0
+
+import PackageDescription
+
+#if os(macOS) || os(iOS) || os(watchOS) || os(tvOS)
+let sysLibrary: Target = Target()
+#else
+let sysLibrary: Target = .systemLibrary(
+                             name: "CLibXML2",
+                             path: "Library/CLibXML2",
+                             pkgConfig: "libxml-2.0",
+                             providers: [
+                                 .brew(["libxml2"]),
+                                 .apt(["libxml2-dev"])
+                             ])
+#endif
+
+let package = Package(
+    name: "SwiftLibXML",
+    products: [
+        .library(
+            name: "SwiftLibXML",
+            targets: ["SwiftLibXML"])
+    ],
+    dependencies: [],
+    targets: [
+        .target(
+            name: "SwiftLibXML",
+            dependencies: ["CLibXML2"]),
+        sysLibrary
+    ],
+    swiftLanguageVersions: [
+        .v4_2
+    ]
+)
